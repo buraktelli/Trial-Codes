2d1bf8a388ead88f61f2cf654b183681ca169594
Burak <brk.telli34@gmail.com>
2021-12-09 10:20:33 +0300

    gdal Deneme eklendi

--- gdalDeneme/index.js
+++ gdalDeneme/index.js
@@ -1,43 +1,31 @@
-var gdal = require("gdal");
-var dataset = gdal.open("./agac_usr.shp");
-var dataset2 = gdal.open("./poligonn_usr.dbf");
-var layer = dataset.layers.get(0);
+// var gdal = require("gdal");
+// var dataset = gdal.open("./agac_usr.shp");
+// // var layer = dataset.layers.get(0);
 
-// console.log("number of features: " + layer.features.count());
-// console.log("fields: " + layer.fields.getNames());
-// console.log("srs: " + (layer.srs ? layer.srs.toWKT() : 'null'));
+// dataset.layers.get(0).features.forEach(function(feature) {
+//     console.log(feature);
+//     const geojson = {}
+//     geojson.type = "Feature"
 
+//     geojson.geometry = JSON.parse(feature.getGeometry().toJSON())
 
+//     geojson.properties = JSON.parse(feature.fields.toJSON())
 
-
-// console.log(layer.features);
-
-
-dataset.layers.get(0).features.forEach(function(feature) {
-
-    const geojson = {}
-
-    geojson.type = "Feature"
-
-
-
-   
-
-
-
-    geojson.geometry = JSON.parse(feature.getGeometry().toJSON())
-
-    geojson.properties = JSON.parse(feature.fields.toJSON())
-
-    console.log(geojson);
-
-});
-// dataset2.layers.get(0).features.forEach(function(feature) {
-
-//     console.log(feature.fields);
-//     feature.fields.forEach((value, key) => {
-//         console.log(value);
-//         console.log(key);
-//     });
-
+//     console.log(geojson);
+//     console.log(geojson.geometry.coordinates);
 // });
+
+
+async function convertToShp() {
+    const { convert } = require('geojson2shp')
+    
+    const options = {
+        layer: 'agac2_usr',
+        targetCrs: 4326
+    }
+    
+    // Paths
+    await convert('./agac.json', './agac2_usr.zip', options)
+}
+
+convertToShp()
