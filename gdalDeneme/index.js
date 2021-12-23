b98524a58fe565154bbcb3df81e0b6c18cb26fd8
Burak <brk.telli34@gmail.com>
2021-12-23 17:13:47 +0300

    Gdal fixed

--- gdalDeneme/index.js
+++ gdalDeneme/index.js
@@ -16,16 +16,52 @@
 // });
 
 
-async function convertToShp() {
-    const { convert } = require('geojson2shp')
-    
-    const options = {
-        layer: 'agac2_usr',
-        targetCrs: 4326
-    }
-    
-    // Paths
-    await convert('./agac.json', './agac2_usr.zip', options)
-}
+// async function convertToShp() {
+//     const { convert } = require('geojson2shp')
 
-convertToShp()
+//     const options = {
+//         layer: 'agac2_usr',
+//         targetCrs: 4326
+//     }
+
+//     // Paths
+//     await convert('./agac.json', './agac2_usr.zip', options)
+// }
+
+// convertToShp()
+
+// var shapefile = require("shapefile");
+
+// shapefile.open("./agac_usr.shp")
+//   .then(source => source.read()
+//     .then(function log(result) {
+//       if (result.done) return;
+//       console.log(result.value.geometry.coordinates);
+//       return source.read().then(log);
+//     }))
+//   .catch(error => console.error(error.stack));
+
+
+const readline = require('readline');
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout,
+    terminal: true
+});
+rl.on('line', (line)=>{
+    console.log(line);
+})
+rl.on('history', (history)=>{
+    console.log('history',history);
+})
+
+var toJSON = require('shp2json');
+
+toJSON.fromShpFile('./agac_usr.shp')
+    .pipe(process.stdout);
+
+// rl.close()
+
+// rl.stdout.on('data', chunk => {
+//     console.log('stdout says', chunk.toString())
+// })
