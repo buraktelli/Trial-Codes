f0d66dd229b2cda1d351ec194dfd5f61ca34c931
Burak <brk.telli34@gmail.com>
2021-12-24 10:08:53 +0300

    Redux-toolkit calismasi

---
+++ redux-toolkit/src/store/index.ts
@@ -0,0 +1,20 @@
+import { configureStore } from "@reduxjs/toolkit";
+import { useDispatch, TypedUseSelectorHook, useSelector } from "react-redux";
+import todoSlice from "../features/todoSlice";
+
+const store = configureStore({
+    reducer: {
+        todos: todoSlice
+    }
+})
+
+export default store;
+
+// Reducerdaki slicelarin typelari otomatik olarak toplanir ve tek bir type olarak RootState e doner
+export type RootState = ReturnType<typeof store.getState>
+
+export type AppDispatch = typeof store.dispatch;
+
+export const useAppDispatch = () => useDispatch<AppDispatch>();
+
+export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector;
